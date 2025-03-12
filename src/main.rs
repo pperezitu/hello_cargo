d6a5069dcb8a7b40246b05625a1a25fd9b1b736b
Pedro Daniel Perez Iturriaga <pedro.pereziturriaga@gmail.com>
2025-03-12 09:49:27 -0300

    primmer commit curso

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+//use rand::random;  // use rand crate, primero va la libreria que es ´rand´ y despues la funcion que se va a usar ´random´
+// use random::Source;
+
+fn main() {
+    // println!("Hello, world! Hola Mucndo!");
+    // let number:u8 = random();
+    // println!("Random number: {}", number);
+
+//    let mut source = random::default(42);
+//    let num:i32 = source.read::<i32>();
+//    println!("Random number: {}", num);
+
+    let x = 8;
+    println!("El valor de nuestra variable x es: {}", x);
+}
